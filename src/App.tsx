c9f93a7eee7146e1e1a06a23504a10a7691f0870
Ken Ng <ng.ken84@gmail.com>
2024-11-25 21:14:25 -0800

    Add Remove Todo Item Functionality

--- src/App.tsx
+++ src/App.tsx
@@ -15,11 +15,16 @@ const App: React.FC = () => {
     })
   }
 
+  const removeTodoItem = (item: TodoItem) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.id != item.id);
+    });
+  }
 
   return (
     <div className="App">
         <TodoItemInput addTodoItem={addTodoItem}/>
-        <TodoList items={todos}/>
+        <TodoList items={todos} removeTodoItem={removeTodoItem}/>
     </div>
   );
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -4,15 +4,21 @@ import TodoItem from '../models/TodoItem';
 import './TodoList.css';
 
 interface TodoListProps {
-    items : TodoItem[]
+    items : TodoItem[],
+    removeTodoItem : (arg0 : TodoItem) => void
 }
 
-const TodoList : React.FC<TodoListProps> = ({items}) => {
-
+const TodoList : React.FC<TodoListProps> = ({items, removeTodoItem}) => {
     
     return (
         <ul className="todo-list">
-            {items.map(todo => <li key={todo.id}>{todo.text}</li>)}
+            {items.map(todo => (
+                <li key={todo.id}>
+                    <span>{todo.text}</span>
+                    <button onClick={() => {
+                        removeTodoItem(todo);
+                    }}>DELETE</button>
+                </li>))}
         </ul>
     );
 }
